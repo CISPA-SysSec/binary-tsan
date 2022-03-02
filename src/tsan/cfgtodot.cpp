84a90617fb49b0a906ecbd64983bbacea39f31fe
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2022-03-02 21:49:02 +0100

    cfg to dot: add function entry and landing pad nodes

--- src/tsan/cfgtodot.cpp
+++ src/tsan/cfgtodot.cpp
@@ -6,44 +6,67 @@
 
 using namespace IRDB_SDK;
 
-static std::size_t blockName(BasicBlock_t *block)
+static std::string blockName(BasicBlock_t *block)
 {
-    return block->getInstructions()[0]->getAddress()->getVirtualOffset();
+    return "node_" + std::to_string(block->getInstructions()[0]->getAddress()->getVirtualOffset());
 }
 
 std::string CFGToDot::createDotFromCFG(const std::unique_ptr<ControlFlowGraph_t> &cfg)
 {
+    std::set<Instruction_t*> landingPads;
+    for (auto block : cfg->getBlocks()) {
+        for (auto instruction : block->getInstructions()) {
+            if (instruction->getEhCallSite() && instruction->getEhCallSite()->getLandingPad()) {
+                landingPads.insert(instruction->getEhCallSite()->getLandingPad());
+            }
+        }
+    }
+
+
     std::stringstream result;
 
     result <<"digraph \"CFG\" {"<<std::endl;
     result <<"size = \"5, 10\";"<<std::endl;
 //    result <<"splines = ortho;"<<std::endl;
     result <<"margin=0;"<<std::endl;
-    result <<"node [shape = rectangle];"<<std::endl;
     for (const auto block : cfg->getBlocks()) {
+        result <<"node [shape = rectangle];"<<std::endl;
         result <<"{"<<std::endl;
-        result <<"\""<<blockName(block)<<"\" [label = \"";
+        result <<blockName(block)<<" [label = \"";
         for (auto instruction : block->getInstructions()) {
             const auto decoded = DecodedInstruction_t::factory(instruction);
             const std::string assembly = disassembly(instruction);
             // align operands after mnemonic
             std::string mnemonic = decoded->getMnemonic();
-            while (mnemonic.size() < 8) {
+            while (mnemonic.size() < 7) {
                 mnemonic.push_back(' ');
             }
             const std::string instructionString = mnemonic + std::string(assembly.begin() + (decoded->getMnemonic().size() + 1), assembly.end());
-            result <<instructionString<<"\\l";
+            result <<std::hex<<instruction->getAddress()->getVirtualOffset()<<": "<<instructionString<<"\\l";
         }
         // select a monospace font to make alignment work
         result <<"\", fontname = \"DejaVu Sans Mono\"];"<<std::endl;
         result <<"}"<<std::endl;
+
+        if (block->getPredecessors().size() == 0) {
+            Instruction_t *instruction = block->getInstructions()[0];
+            const bool isLandingPad = landingPads.find(instruction) != landingPads.end();
+            const bool isFunctionEntry = cfg->getFunction()->getEntryPoint() == instruction;
+            if (isLandingPad || isFunctionEntry) {
+                const std::string nodeName = isLandingPad ? "\"landing pad\"" : "\"function entry\"";
+                result <<"node [shape = ellipse];"<<std::endl;
+                result <<"{ "<<nodeName<<" }"<<std::endl;
+                result <<"edge [style = solid];"<<std::endl;
+                result <<nodeName<<" -> "<<blockName(block)<<std::endl;
+            }
+        }
     }
     result <<"edge [style = solid];"<<std::endl;
     for (const auto block : cfg->getBlocks()) {
         for (const auto succ : block->getSuccessors()) {
             const auto decoded = DecodedInstruction_t::factory(block->getInstructions().back());
             std::string color = "darkblue";
-            if (decoded->isBranch()) {
+            if (decoded->isBranch() && !decoded->isCall()) {
                 const auto edgeTypes = cfg->getEdgeType(block, succ);
                 const auto it = edgeTypes.find(CFGEdgeTypeEnum::cetFallthroughEdge);
                 if (it != edgeTypes.end()) {
@@ -52,7 +75,7 @@ std::string CFGToDot::createDotFromCFG(const std::unique_ptr<ControlFlowGraph_t>
                     color = "darkgreen";
                 }
             }
-            result <<"\""<<blockName(block)<<"\" -> \""<<blockName(succ)<<"\" [color = \""<<color<<"\"];"<<std::endl;
+            result <<blockName(block)<<" -> "<<blockName(succ)<<" [color = \""<<color<<"\"];"<<std::endl;
         }
     }
     result <<"}"<<std::endl;
