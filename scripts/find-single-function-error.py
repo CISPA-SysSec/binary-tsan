5c6b0b98625cab50a927c816547f1699d5311135
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2022-03-11 14:56:55 +0100

    Add manual check mode to single function error finder

--- scripts/find-single-function-error.py
+++ scripts/find-single-function-error.py
@@ -14,22 +14,36 @@ outFile = "instrumented-libraries/libaws.so.5"
 runFile = ["./wps-mod"]
 timeoutIsFailure = False
 iterations = 1
+manualCheck = True
+
 
 def checkError():
-    environmentVariables = dict(os.environ)
-    environmentVariables["LD_LIBRARY_PATH"] = "/home/andi/Masterarbeit/binary-tsan/build/instrumented-libraries"
-    environmentVariables["TSAN_OPTIONS"] = "report_signal_unsafe=0"
-    for i in range(iterations):
-        try: 
-            res = subprocess.run(runFile, env=environmentVariables, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=50)
-            hasProblem = res.returncode != 0
-            outStr = str(res.stdout)
-            if "ThreadSanitizer: SEGV" in outStr or "Aborted" in outStr or "FATAL: ThreadSanitizer CHECK failed:" in outStr or "raised " in outStr:
+    if manualCheck:
+        # alert the user, will only work when package "sox" is installed
+        os.system("play -q -n synth 1 sin 880")
+        while True:
+            correctString = input("Please check for errors manually, report Error/Fine: ")
+            if correctString == "Error":
                 return True
-        except:
-            print("Execution timeout")
-            if timeoutIsFailure:
-                return True
+            if correctString == "Fine":
+                return False
+            print("Could not parse answer")
+        
+    else:
+        environmentVariables = dict(os.environ)
+        environmentVariables["LD_LIBRARY_PATH"] = "/home/andi/Masterarbeit/binary-tsan/build/instrumented-libraries"
+        environmentVariables["TSAN_OPTIONS"] = "report_signal_unsafe=0"
+        for i in range(iterations):
+            try: 
+                res = subprocess.run(runFile, env=environmentVariables, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=50)
+                hasProblem = res.returncode != 0
+                outStr = str(res.stdout)
+                if "ThreadSanitizer: SEGV" in outStr or "Aborted" in outStr or "FATAL: ThreadSanitizer CHECK failed:" in outStr or "raised " in outStr:
+                    return True
+            except:
+                print("Execution timeout")
+                if timeoutIsFailure:
+                    return True
     return False
 
 def checkFunctions(functionNames):
