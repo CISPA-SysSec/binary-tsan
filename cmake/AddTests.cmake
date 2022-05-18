da4c6931e47630e3a75b93d8c4e4509e7d3c3a5f
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2022-05-18 11:23:01 +0200

    Only use c-testsuite when tmsu is installed

--- cmake/AddTests.cmake
+++ cmake/AddTests.cmake
@@ -2,9 +2,11 @@ add_test(NAME run-tsan-tests
 	COMMAND python3 "${CMAKE_SOURCE_DIR}/scripts/run-tsan-tests.py" "./thread-sanitizer.sh" "tsan-test-output/"
 	WORKING_DIRECTORY "${CMAKE_BUILD_DIRECTORY}")
 
-add_test(NAME c-testsuite
-	COMMAND ./single-exec clang-tsan-rewrite
-	WORKING_DIRECTORY "${C_TESTSUITE_DIRECTORY}")
+if(HAVE_C_TESTSUITE)
+    add_test(NAME c-testsuite
+        COMMAND ./single-exec clang-tsan-rewrite
+        WORKING_DIRECTORY "${C_TESTSUITE_DIRECTORY}")
+endif()
 
 # show what went wrong by default
 add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
--- cmake/GetCTestsuite.cmake
+++ cmake/GetCTestsuite.cmake
@@ -3,17 +3,32 @@ include(ExternalProject)
 set(TESTSUITE_PATCH_FILE ${CMAKE_BINARY_DIR}/c-testsuite.patch)
 configure_file(${CMAKE_SOURCE_DIR}/cmake/c-testsuite.patch.in ${TESTSUITE_PATCH_FILE} @ONLY)
 
-ExternalProject_Add(project_c_testsuite
-    GIT_REPOSITORY https://github.com/c-testsuite/c-testsuite.git
-    GIT_TAG 5c7275656d751de0e68b2d340a95b5681858ed07
-    PATCH_COMMAND cat ${TESTSUITE_PATCH_FILE} | patch -p1
-    BUILD_IN_SOURCE TRUE
-    CONFIGURE_COMMAND tmsu init && ./scripts/make-search-index
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-)
+find_program(TMSU_LOCATION NAMES tmsu)
 
-externalproject_get_property(project_c_testsuite source_dir)
-set(C_TESTSUITE_DIRECTORY ${source_dir})
-set_target_properties(project_c_testsuite PROPERTIES EXCLUDE_FROM_ALL true)
+if(TMSU_LOCATION)
 
+    set(HAVE_C_TESTSUITE TRUE)
+
+    ExternalProject_Add(project_c_testsuite
+        GIT_REPOSITORY https://github.com/c-testsuite/c-testsuite.git
+        GIT_TAG 5c7275656d751de0e68b2d340a95b5681858ed07
+        PATCH_COMMAND cat ${TESTSUITE_PATCH_FILE} | patch -p1
+        BUILD_IN_SOURCE TRUE
+        CONFIGURE_COMMAND tmsu init && ./scripts/make-search-index
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+    )
+
+    externalproject_get_property(project_c_testsuite source_dir)
+    set(C_TESTSUITE_DIRECTORY ${source_dir})
+    set_target_properties(project_c_testsuite PROPERTIES EXCLUDE_FROM_ALL true)
+
+else()
+
+    message("tmsu is not installed, c-testsuite will be run")
+
+    set(HAVE_C_TESTSUITE FALSE)
+
+    add_custom_target(project_c_testsuite)
+
+endif()
